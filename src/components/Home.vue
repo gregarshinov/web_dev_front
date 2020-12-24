27974904401ddefab47d9dcc64661744b9b28c75
Greg Arshinov <gregarshinov@hotmail.com>
2020-12-24 23:36:40 +0300

    base arch

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="container">
+<slot>The page that you are looking for missing</slot>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Base"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Research.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Research"
+}
+</script>
+
+<style scoped>
+
+</style>
