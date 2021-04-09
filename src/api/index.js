840dae149496cd44b4140347f6705fbbc9acfb34
Greg Arshinov <gregarshinov@hotmail.com>
2021-04-10 02:14:36 +0300

    Fixed the bug with the wrong url formation

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = 'http://127.0.0.1:5000/api'
+const API_URL = 'http://127.0.0.1:5000/api/'
 
 export async function fetchSuggestions(text) {
     return axios.post(API_URL + "get_suggestions", {text: text})
